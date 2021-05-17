7311c3d475670fe641d13e46bbe11cb32e9ee140
Edward Ramirez <edal_ramirez@hotmail.com>
2021-05-17 16:07:59 -0500

    Updating App Component
    The app component is the only one that I'm using for the moment. This component has state: the incoming tasks list
    form the API and the text shown in the input. The useEffect hook gets the tasks from the REST API using fetch,
    it parses the response as JSON and uses setTasks to update the state, so the template can render the tasks list.
    
    The onSubmit form attribute triggers the submitTask function that creates a new object and send it to the API through
    a POST request. Then, inside the Promise, it changes the state pushing the data item in the array and using setTasks.

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState(null);
+  const [text, setText] = useState("");
+  var controller = new AbortController();
+  var signal = controller.signal;
+
+  useEffect(() => {
+    fetch('http://edwardramirez.pythonanywhere.com/', {
+      signal
+    })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("HTTP ERROR: " + response.status);
+        }
+      })
+      .then(data => {
+        setTasks(data);
+        console.log(data);
+      })
+      .catch(err => {
+        if (err.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          console.log(err);
+        }
+      });
+    
+    return () => {
+      controller.abort();
+    }
+  }, []);
+
+  const submitTask = (e) => {
+    e.preventDefault();
+    const newTask = { text }
+    console.log(newTask);
+    setText("");
+    fetch('http://edwardramirez.pythonanywhere.com/', {
+      method: 'POST', 
+      headers: { 'Content-Type': 'application/json' }, 
+      body: JSON.stringify(newTask)
+    })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("HTTP ERROR: " + response.status);
+        }
+      })
+      .then(data => {
+        console.log(data);
+        tasks.push(data);
+        setTasks([...tasks])
+      })
+      .catch(err => {
+        if (err.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          console.log(err);
+        }
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="task-container">
+        <div id="form-wrapper">
+          <form id="form" onSubmit={submitTask}>
+            <div className="flex-wrapper">
+              <div style={{ flex: 6 }} >
+                <input type="text" className="form-control" id="text" placeholder="Add a task" value={text} onChange={(e) => setText(e.target.value)}/>
+              </div>
+              <div style={{flex: 1 }}>
+                <input type="submit" id="submit" className="btn btn-warning" value="Submit"/>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div id="list-wrapper">
+          {tasks && tasks.map((task) => (
+            <div className="task-wrapper flex-wrapper" key={task.id}>
+              <div style={{flex: 7}}>
+                <span>{task.text }</span>
+              </div>
+              <div style={{flex: 1}}>
+                <button className="btn btn-sm btn-outline-info">Edit</button>
+              </div>
+              <div style={{flex: 1}}>
+                <button className="btn btn-sm btn-outline-dark delete">-</button>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
