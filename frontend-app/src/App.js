5babdcbfd62357591f18a236790825f81c1e085f
Edward Ramirez <edal_ramirez@hotmail.com>
2021-05-17 18:04:11 -0500

    Edit and Delete Features
    EDITING: The button triggers 'handleEdit' function. This is going to set the text state with the task content, the editing state as True and
    ActiveItem with the task passed through the parameters. Then, the onSubmit function checks if edit is true and sends a PUT request to the
    corresponding url. Finally, this request returns an object with the updated data and the tasks list is updated with the new information.
    
    DELETE: the handleDelete function deletes the task from the list and updates the state. Then, it makes a DELETE request and deletes the
    database record.

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState(null);
+  const [activeItem, setActiveItem] = useState(null);
+  const [editing, setEditing] = useState(false);
   const [text, setText] = useState("");
   var controller = new AbortController();
   var signal = controller.signal;
@@ -40,34 +42,72 @@ function App() {
   const submitTask = (e) => {
     e.preventDefault();
     const newTask = { text }
-    console.log(newTask);
     setText("");
-    fetch('http://edwardramirez.pythonanywhere.com/', {
-      method: 'POST', 
-      headers: { 'Content-Type': 'application/json' }, 
-      body: JSON.stringify(newTask)
-    })
-      .then(response => {
-        console.log(response);
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("HTTP ERROR: " + response.status);
-        }
+
+    if (editing) {
+      fetch(`http://edwardramirez.pythonanywhere.com/task/${activeItem.id}/`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newTask)
       })
-      .then(data => {
-        console.log(data);
-        tasks.push(data);
-        setTasks([...tasks])
+        .then(response => {
+          console.log(response);
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("HTTP ERROR: " + response.status);
+          }
+        })
+        .then(data => {
+          console.log(data);
+          setTasks(tasks.map((task) => task === activeItem ? data : task));
+        })
+        .catch(err => console.log(err));
+      setEditing(false);
+    } else {
+      fetch('http://edwardramirez.pythonanywhere.com/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newTask)
       })
-      .catch(err => {
-        if (err.name === "AbortError") {
-          console.log("Fetch aborted");
-        } else {
-          console.log(err);
-        }
-      });
-  }
+        .then(response => {
+          console.log(response);
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("HTTP ERROR: " + response.status);
+          }
+        })
+        .then(data => {
+          console.log(data);
+          tasks.push(data);
+          setTasks([...tasks])
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    };
+  };
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+
+    fetch(`http://edwardramirez.pythonanywhere.com/task/${id}/`, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'}
+      })
+        .then(response => response.ok ? response.text() : console.log('error:' + response.status))
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => console.log(err));
+  };
+  
+  const handleEdit = (task) => {
+    setText(task.text);
+    setEditing(true);
+    setActiveItem(task)
+  };
 
   return (
     <div className="container">
@@ -87,15 +127,14 @@ function App() {
         <div id="list-wrapper">
           {tasks && tasks.map((task) => (
             <div className="task-wrapper flex-wrapper" key={task.id}>
-              <div style={{flex: 7}}>
+              <div style={{ flex: 7, textDecorationLine: task.completed ?'line-through': 'none' }}>
                 <span>{task.text }</span>
               </div>
               <div style={{flex: 1}}>
-                <button className="btn btn-sm btn-outline-info">Edit</button>
+                <button className="btn btn-sm btn-outline-info" onClick={() => handleEdit(task)}>Edit</button>
               </div>
               <div style={{flex: 1}}>
-                <button className="btn btn-sm btn-outline-dark delete">-</button>
-
+                <button className="btn btn-sm btn-outline-dark delete" onClick={() => handleDelete(task.id)}>-</button>
               </div>
             </div>
           ))}
